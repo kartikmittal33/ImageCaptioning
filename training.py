970d4f8d00ee2d945bf8b75accd27c65100d2c4b
Shiv Paul <shivapaul98@gmail.com>
2020-11-29 00:31:20 -0500

    Started Training

--- training.py
+++ training.py
@@ -3,6 +3,11 @@ from keras.layers import LSTM, Embedding, TimeDistributed, Dense, RepeatVector, 
 import cPickle as pickle
 import numpy as np
 import pandas as pd
+from keras.preprocessing import sequence
+from keras.callbacks import ModelCheckpoint
+
+BATCH_SIZE = 32
+EPOCHS = 64
 
 
 class CaptionGenerator():
@@ -16,7 +21,7 @@ class CaptionGenerator():
 		self.index_word = {}
 		self.word_index = {}
 		self.num_samples = 0
-		self.encoded_images = pickle.load(open("encoded_images.p", "rb"))
+		self.encoded_images = pickle.load(open("Flickr_Data/encoded_images.p", "rb"))
 
 		df = pd.read_csv('Flickr_Data/Flickr_Dataset/flickr_8k_train_dataset.txt', delimiter='\t')
 
@@ -27,18 +32,19 @@ class CaptionGenerator():
 		for text in captions:
 			self.num_samples += len(text.split()) - 1
 
-		words = [caption.split() for caption in captions]
+		captions_split = [caption.split() for caption in captions]
 
 		unique = set()
-		for word in words:
-			unique.add(word)
+		for words in captions_split:
+			for word in words:
+				unique.add(word)
 
 		unique = list(unique)
 		self.vocab_size = len(unique)
 
-		for i, word in enumerate(unique):
-			self.word_index[word] = i
-			self.index_word[i] = word
+		for i, words in enumerate(unique):
+			self.word_index[words] = i
+			self.index_word[i] = words
 
 		max_caption_length = 0
 		for caption in captions:
@@ -48,12 +54,12 @@ class CaptionGenerator():
 
 		print("Training initialization finished")
 
-
 	def make_model(self):
 		print("Making model")
 
 		image_model = Sequential()
 		image_model.add(Dense(128, input_dim=4096, activation='relu'))
+		image_model.add(RepeatVector(self.max_caption_length))
 
 		caption_model = Sequential()
 		caption_model.add(Embedding(self.vocab_size, 256, input_length=self.max_caption_length))
@@ -66,7 +72,81 @@ class CaptionGenerator():
 		main_model.add(Dense(self.vocab_size))
 		main_model.add(Activation('softmax'))
 
-		main_model.compile(loss='categorical_crossentropy', optimizer='adamoptimizer', metrics=['accuracy'])
+		main_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 		print("Model making finished")
 		return main_model
+
+	def generate_data(self, batch_size=32):
+
+		partial_captions = []
+		next_words = []
+		images = []
+		df = pd.read_csv('Flickr_Data/Flickr_Dataset/flickr_8k_train_dataset.txt', delimiter='\t')
+
+		captions = []
+		imgs = []
+		for index, row in df.iterrows():
+			captions.append(row[1][1])
+			imgs.append(row[1][0])
+
+		total_count = 0
+
+		while 1:
+			image_counter = -1
+
+			for caption in captions:
+				image_counter += 1
+
+				if imgs[image_counter] in self.encoded_images:
+					current_image = self.encoded_images[imgs[image_counter]]
+
+					for i in range(len(caption.split()) - 1):
+						total_count += 1
+
+						partial = [self.word_index[word] for word in caption.split()[:i + 1]]
+						partial_captions.append(partial)
+
+						next = np.zeros(self.vocab_size)
+						next[self.word_index[caption.split()[i + 1]]] = 1
+						next_words.append(next)
+
+						images.append(current_image)
+
+						if total_count >= batch_size:
+							next_words = np.asarray(next_words)
+							images = np.asarray(images)
+							partial_captions = sequence.pad_sequences(partial_captions, maxlen=self.max_caption_length,
+																	padding='post')
+							total_count = 0
+
+							yield [[images, partial_captions], next_words]
+
+							partial_captions = []
+							next_words = []
+							images = []
+
+
+if __name__ == '__main__':
+
+	print("Starting training")
+
+	caption_generator = CaptionGenerator()
+	model = caption_generator.make_model()
+
+	counter = 0
+	file = "Flickr_Data/weights-{epoch:02d}.hdf5"
+
+	checkpoint = ModelCheckpoint(file, monitor='loss', verbose=1, save_best_only=True, mode='min')
+	callbacks_list = [checkpoint]
+
+	model.fit_generator(caption_generator.generate_data(batch_size=BATCH_SIZE),
+						steps_per_epoch=caption_generator.num_samples / BATCH_SIZE,
+						epochs=EPOCHS, verbose=2, callbacks=callbacks_list)
+	try:
+		model.save("Model/model.h5", overwrite=True)
+		model.save_weights("Model/weights.h5", overwrite=True)
+	except:
+		print("Could not save model")
+
+	print("Training finished, model and weights saved in folder \'Model\'")
